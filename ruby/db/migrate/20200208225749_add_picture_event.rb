e55b6c994cf0c37a8ee20ca60035379d5de378f7
Aitor garcia <bbminze@gmail.com>
2020-02-09 00:00:10 +0100

    add_picture

---
+++ ruby/db/migrate/20200208225749_add_picture_event.rb
@@ -0,0 +1,5 @@
+class AddPictureEvent < ActiveRecord::Migration[6.0]
+  def change
+    add_column :events, :picture, :string
+  end
+end
--- ruby/db/schema.rb
+++ ruby/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_08_222732) do
+ActiveRecord::Schema.define(version: 2020_02_08_225749) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2020_02_08_222732) do
     t.decimal "lon"
     t.decimal "lat"
     t.string "address"
+    t.string "picture"
     t.index ["associations_id"], name: "index_events_on_associations_id"
     t.index ["categories_id"], name: "index_events_on_categories_id"
   end
