33770e6e5d8c651259e4e8ff2c8322559cfba3ba
Aitor garcia <bbminze@gmail.com>
2020-02-08 14:12:45 +0100

    default environment

--- ruby/app/controllers/api/main_controller.rb
+++ ruby/app/controllers/api/main_controller.rb
@@ -4,7 +4,7 @@
 class Api::MainController < ApplicationController
 
   def test
-    render json: {:working => true, params: params[:id]}
+    render json: {:working => true, params: params[:id], :env => ENV['testing_env']}
   end
 
 end
