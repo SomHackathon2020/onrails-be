7ab5f3f9a9f4cdaf108179222ae7a18567c2ffd7
Jan Cano Lopera <jan.ml@hotmail.com>
2020-02-09 01:46:04 +0100

    Ya va (creo)

--- ruby/app/models/Achievement.rb
+++ ruby/app/models/Achievement.rb
@@ -1,4 +1,4 @@
 class Achievement < ActiveRecord::Base
-  has_many :users_list, foreign_key: :users_id, class_name: 'AchievementAssignation'
-  has_many :users, through: :users_list
+  has_many :users_list, class_name: 'AchievementAssignation', foreign_key: :users_id
+  has_many :users, class_name:"User",  through: :users_list
 end
